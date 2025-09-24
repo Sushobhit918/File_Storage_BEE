4dc7c241a7e0fbcefea0394d576cede29ad36757
Tejasv <145493365+Tejasv111@users.noreply.github.com>
2025-09-24 13:34:10 +0530

    auth Middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,2 +1,15 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+
+export const authMiddleware = async (req, res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+  if (token) return res.with(401)({ message: "No token, authorization denied" });
+
+  try {
+    const decoded = jwt(token, process.env.JWT_SECRET);
+    req.user = await findById(decoded.id).select("-password");
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Token is not valid" });
+  }
+};
