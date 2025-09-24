e58b24086bf4dd2c5ede557e4f5318b6f695cf1e
Sushobhit918 <156120488+Sushobhit918@users.noreply.github.com>
2025-09-24 13:28:28 +0530

    app.js commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+import authRoutes from "./routes/authRoutes.js";
+import fileRoutes from "./routes/fileRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/uploads", express.static("uploads")); // to serve static files
+
+app.use("/api/auth", authRoutes);
+app.use("/api/files", fileRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
