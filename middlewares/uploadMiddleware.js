cc5d6da0cac9627ea0166b0bef547b453a53d00e
Tejasv <145493365+Tejasv111@users.noreply.github.com>
2025-09-24 13:31:50 +0530

    photo storage

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -1,2 +1,9 @@
 import multer from "multer";
 import path from "path";
+
+// Photos storage
+const photoStorage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "uploads/photos"),
+  filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname),
+});
+
